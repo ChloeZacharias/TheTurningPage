accff6846b8253e8c2f359facb04fd1f3c2da931
ChloeZacharias <chloe.m.zacharias@gmail.com>
2025-12-06 12:19:19 -0800

    fixing scripts

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // Must be 'dist', not root
+    emptyOutDir: true,
+  },
   base: "/TheTurningPage/",
 });
