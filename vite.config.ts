e768f60631765588ca2e7e09d60b435ecab50b83
ChloeZacharias <chloe.m.zacharias@gmail.com>
2025-12-06 12:23:16 -0800

    WIP fixing script

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  root: "src", // ensure Vite doesn’t write to root
   build: {
-    outDir: "dist", // Must be 'dist', not root
+    outDir: "../dist", // relative to src/, final build goes to dist
     emptyOutDir: true,
   },
   base: "/TheTurningPage/",
